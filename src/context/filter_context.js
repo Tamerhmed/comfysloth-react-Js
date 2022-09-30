68252864341f361d4e9736b97db98945df508c73
Tamerhmed <tamer30230@gmail.com>
2022-09-30 14:19:13 +1000

    grid view filter

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -29,7 +29,12 @@ export const FilterProvider = ({ children }) => {
   }, [products]);
   
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider 
+    value={
+      {
+        ...state
+      }
+    }>
       {children}
     </FilterContext.Provider>
   )
